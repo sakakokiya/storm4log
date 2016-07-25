81f6f2f40d77674d6dfea2ceb35ea3bf24ef6258
sakakokiya <sakakokiya@163.com>
2016-07-25 18:18:44 +0800

    mysql-log-test

---
+++ mysql-parser-master/src/test/java/ParserJsonTest.java
@@ -0,0 +1,28 @@
+import com.github.hackerwin7.mysql.parser.protocol.json.ConfigJson;
+import net.sf.json.JSONObject;
+import com.github.hackerwin7.mysql.parser.parser.utils.ParserConfig;
+
+/**
+ * Created by hp on 14-11-14.
+ */
+public class ParserJsonTest {
+
+    public static void main(String[] args) {
+        ParserConfig configer = new ParserConfig();
+        ConfigJson configJson = new ConfigJson("jd-mysql-parser-1");
+        JSONObject jRoot = configJson.getJson();
+        if(jRoot != null) {
+            JSONObject jContent = jRoot.getJSONObject("info").getJSONObject("content");
+            configer.setHbaseRootDir(jContent.getString("HbaseRootDir"));
+            configer.setHbaseDistributed(jContent.getString("HbaseDistributed"));
+            configer.setHbaseZkQuorum(jContent.getString("HbaseZKQuorum"));
+            configer.setHbaseZkPort(jContent.getString("HbaseZKPort"));
+            configer.setDfsSocketTimeout(jContent.getString("DfsSocketTimeout"));
+        }
+
+        System.out.println(configer.getHbaseRootDir()+","+configer.getHbaseDistributed()+"," +
+                configer.getHbaseZkQuorum()+","+configer.getHbaseZkPort()+","+configer.getDfsSocketTimeout());
+
+    }
+
+}
